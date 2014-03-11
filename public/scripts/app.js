c5ef2732adf81764463a62e0630fae3003d81160
jgpettibone <jeanette.pettibone@gmail.com>
2014-03-11 07:28:57 -0700

    Removed config information from app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,4 @@
 var jsvis = angular.module('jsvis', ['ngRoute','ngAnimate'])
-  .config(function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $routeProvider
-      .when('/', {
-        templateUrl: '../views/mainindex.html',
-        controller: 'MainController'
-      })
-      .when('/about', {
-        templateUrl: '../views/mainindex.html',
-        controller: 'MainController'
-      })
-      .when('/contact', {
-        templateUrl: '../views/mainindex.html',
-        controller: 'MainController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
   .controller('MainController', function($scope, $interval, ScopeService) {
     $scope.codeText = '';
     $scope.prevStatement = '';
